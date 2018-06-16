00ccd0eecdd4171ca1e0ec2931ef682ce3f5b1d7
alper <alperkay@hotmail.com>
2018-06-16 09:50:37 +0200

    initial boilerplate setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <h1>Welcome to the Hangman Game!</h1>
+        <h2>Start playing!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
